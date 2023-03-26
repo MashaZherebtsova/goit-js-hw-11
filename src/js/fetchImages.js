914cd45f65eaf6c7a45826bc9aa89b573aa55990
MashaZherebtsova <113789649+MashaZherebtsova@users.noreply.github.com>
2023-03-26 17:41:50 +0300

    unit26.03

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,20 +2,14 @@ import axios from 'axios';
 
 
 
-export const fetchImages = async (inputValue, pageNr) => {
-    return await fetch(
-        `https://pixabay.com/api/?key=34748858-d31d3a783fed40cd5b22b2555&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-    )
-    .then(async response => {
-        if (!response.ok) {
-            if (response.status === 404) {
-                return [];
-            }
-            throw new Error(response.status);
-        }
-        return await response.json();
-    })
-    .catch(error=> {
-        console.error(error);
-    });
-}
+export async function fetchImages(value, page) {
+    const url = 'https://pixabay.com/api/';
+    const key = '34748858-d31d3a783fed40cd5b22b2555';
+    const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  
+    return await axios.get(`${url}${filter}`).then(response => response.data);
+  }
+
+
+
+
